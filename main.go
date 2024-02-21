daf67a8524812eb4c0b11ab5ab6c84f8ea2880fa
Grégoire Détrez <gregoire@fripost.org>
2024-02-22 00:32:42 +0100

    Remove opentelemetry

--- main.go
+++ main.go
@@ -3,35 +3,15 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"time"
-
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
-
-var tracer = otel.Tracer("github.com/gdetrez/bookmaker")
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
-
-	if shutdownTracing, err := initTracing(); err != nil {
-		log.Printf("Error initializing tracing: %v", err)
-	} else {
-		defer shutdownTracing(context.Background())
-	}
 
 	addr := ":8024"
 	log.Printf("Starting HTTP server at %s", addr)
@@ -41,7 +21,7 @@ func main() {
 	mux.Handle("/", http.HandlerFunc(Index))
 	srv := http.Server{
 		Addr:    addr,
-		Handler: otelhttp.NewHandler(mux, "request"),
+		Handler: mux,
 	}
 	go func() {
 		err := srv.ListenAndServe()
@@ -57,46 +37,4 @@ func main() {
 		log.Println(err)
 	}
 	log.Println("Goodbye")
-}
-
-func initTracing() (func(context.Context) error, error) {
-	ctx := context.Background()
-
-	res, err := resource.New(ctx,
-		resource.WithAttributes(semconv.ServiceName("bookmaker")),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create resource: %w", err)
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, time.Second)
-	defer cancel()
-	conn, err := grpc.DialContext(ctx, "jaeger:4317",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithBlock(),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
-	}
-
-	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
-	if err != nil {
-		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
-	}
-
-	// Register the trace exporter with a TracerProvider, using a batch
-	// span processor to aggregate spans before export.
-	bsp := trace.NewBatchSpanProcessor(traceExporter)
-	tracerProvider := trace.NewTracerProvider(
-		trace.WithSampler(trace.AlwaysSample()),
-		trace.WithResource(res),
-		trace.WithSpanProcessor(bsp),
-	)
-	otel.SetTracerProvider(tracerProvider)
-
-	// set global propagator to tracecontext (the default is no-op).
-	otel.SetTextMapPropagator(propagation.TraceContext{})
-
-	// Shutdown will flush any remaining spans and shut down the exporter.
-	return tracerProvider.Shutdown, nil
 }
